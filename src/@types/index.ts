ce162304b8a443949a4d243577ea7bfa20678c6e
trastanechora <trastanechora@gmail.com>
2021-03-05 22:06:47 +0700

    Update typings directory

---
+++ src/@types/index.ts
@@ -0,0 +1,8 @@
+import { Store } from 'vuex';
+
+/* -------------------------------------------------
+=> This is necessary for direct import
+=> e.g. import { SidebarMenuItem } from './@types'
+------------------------------------------------- */
+export default Store;
+export * from './ui';
