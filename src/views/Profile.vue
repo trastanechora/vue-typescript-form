825dd93ef827e90a00f84f8084263f1be56b47b1
trastanechora <trastanechora@gmail.com>
2021-03-05 22:08:27 +0700

    Add profile page

---
+++ src/views/Profile.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="profile">
+    <v-card max-width="500" class="mx-auto mt-10">
+      <v-list>
+        <v-list-item>
+          <v-list-item-avatar>
+            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>John Leider</v-list-item-title>
+            <v-list-item-subtitle>Founder of Vuetify</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-btn :class="fav ? 'red--text' : ''" icon @click="fav = !fav">
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item>
+          <v-list-item-action>
+            <v-switch v-model="message" color="purple"></v-switch>
+          </v-list-item-action>
+          <v-list-item-title>Enable messages</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-action>
+            <v-switch v-model="hints" color="purple"></v-switch>
+          </v-list-item-action>
+          <v-list-item-title>Enable hints</v-list-item-title>
+        </v-list-item>
+      </v-list>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn text @click="menu = false">
+          Cancel
+        </v-btn>
+        <v-btn color="primary" text @click="menu = false">
+          Save
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
