b25febdd4c832b34aedf14c0dbc45bd23848dbf1
trastanechora <trastanechora@gmail.com>
2021-03-06 09:43:02 +0700

    Add sample chart

---
+++ src/components/ChartBar.vue
@@ -0,0 +1,22 @@
+<script>
+import { Bar } from 'vue-chartjs';
+
+export default {
+  extends: Bar,
+  mounted() {
+    this.renderChart(
+      {
+        labels: ['Angular', 'React', 'Vue', 'Svelte'],
+        datasets: [
+          {
+            label: 'Vote',
+            backgroundColor: ['#b02f28', '#00D8FF', '#2f993d', '#ad5a17'],
+            data: [20, 25, 30, 15]
+          }
+        ]
+      },
+      { responsive: true, maintainAspectRatio: false }
+    );
+  }
+};
+</script>
---
+++ src/components/ChartPie.vue
@@ -0,0 +1,21 @@
+<script>
+import { Pie } from 'vue-chartjs';
+
+export default {
+  extends: Pie,
+  mounted() {
+    this.renderChart(
+      {
+        labels: ['Angular', 'React', 'Vue', 'Svelte'],
+        datasets: [
+          {
+            backgroundColor: ['#b02f28', '#00D8FF', '#2f993d', '#ad5a17'],
+            data: [20, 25, 30, 15]
+          }
+        ]
+      },
+      { responsive: true, maintainAspectRatio: false }
+    );
+  }
+};
+</script>
