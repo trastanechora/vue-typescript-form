e72dcc63faf1028982b096b4091a285297de6a63
trastanechora <trastanechora@gmail.com>
2021-03-13 00:10:38 +0700

    Develop add and edit question dialog

---
+++ src/components/AddQuestion.vue
@@ -0,0 +1,214 @@
+<template>
+  <v-dialog persistent v-model="dialog" transition="dialog-bottom-transition" max-width="600">
+    <template>
+      <v-card>
+        <v-toolbar color="primary" dark>Tambahkan Pertanyaan</v-toolbar>
+        <v-card-text>
+          <div class="mt-4">
+            <v-form ref="addQuestionForm" v-model="valid" lazy-validation class="pa-0">
+              <v-card-text>
+                <v-layout row>
+                  <v-flex lg12 sm12 xs12>
+                    <v-select
+                      v-model="currentQuestion.type"
+                      outlined
+                      clearable
+                      :items="questionTypeList"
+                      item-text="label"
+                      item-value="value"
+                      label="Tipe Pertanyaan"
+                      class="required"
+                      :rules="notEmpty('Tipe Pertanyaan')"
+                      :disabled="isLoading"
+                      return-object
+                      :loading="isLoading"
+                    ></v-select>
+                  </v-flex>
+                  <v-flex lg12 sm12 xs12>
+                    <v-text-field
+                      v-model="currentQuestion.text"
+                      outlined
+                      clearable
+                      label="Pertanyaan"
+                      type="text"
+                      autocomplete="off"
+                      class="required"
+                      :rules="notEmpty('Pertanyaan')"
+                      :disabled="isLoading"
+                      :loading="isLoading"
+                    ></v-text-field>
+                  </v-flex>
+                  <v-flex lg12 sm12 xs12>
+                    <v-layout v-if="currentQuestion.type === 'select'">
+                      <v-flex lg12 sm12 xs12>
+                        <v-text-field
+                          v-model="currentQuestion.text"
+                          outlined
+                          clearable
+                          label="Pertanyaan"
+                          type="text"
+                          autocomplete="off"
+                          class="required"
+                          :rules="notEmpty('Pertanyaan')"
+                          :disabled="isLoading"
+                          :loading="isLoading"
+                        ></v-text-field>
+                      </v-flex>
+                    </v-layout>
+                  </v-flex>
+                </v-layout>
+              </v-card-text>
+            </v-form>
+          </div>
+        </v-card-text>
+        <v-card-actions class="justify-end py-4 px-6">
+          <v-switch v-model="currentQuestion.required" label="Harus diisi" />
+          <v-spacer />
+          <v-btn text @click="close" class="error--text" :disabled="isLoading" :loading="isLoading">Batal</v-btn>
+          <v-btn v-if="isEdit" text @click="editQuestion" :disabled="isLoading" :loading="isLoading">Edit</v-btn>
+          <v-btn v-else text @click="addQuestion" :disabled="isLoading" :loading="isLoading">Tambahkan</v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Emit, Watch } from 'vue-property-decorator';
+import { VForm, Question, QuestionType, QuestionTypeObject } from '@/@types';
+import { notEmptyRules } from '@/@utils';
+@Component
+export default class DialogQuestion extends Vue {
+  /* ------------------------------------
+  => Props declaration
+  ------------------------------------ */
+  @Prop({ required: true, type: Boolean })
+  readonly dialog!: boolean;
+  @Prop({ required: true, type: Boolean })
+  readonly isEdit!: boolean;
+  @Prop({ required: true, type: String })
+  readonly questionKey!: string;
+  @Prop({ required: true, type: Object })
+  readonly selectedQuestion!: Question;
+  @Emit('add')
+  add(item: Question): Question {
+    return item;
+  }
+  @Emit('edit')
+  edit(item: Question): Question {
+    return item;
+  }
+
+  @Prop({
+    required: false,
+    type: Function,
+    default() {
+      return true;
+    }
+  })
+  readonly closeDialog!: FunctionConstructor;
+
+  /* ------------------------------------
+  => Local State Declaration
+  ------------------------------------ */
+  valid: boolean = true;
+  currentQuestion: Question = {
+    key: '',
+    type: {
+      label: '',
+      value: QuestionType.EMPTY
+    },
+    required: false,
+    text: ''
+  };
+
+  /* ------------------------------------
+  => Setter and Getter
+  ** (Adopt store variables to local state)
+  ------------------------------------ */
+  get isLoading(): boolean {
+    return this.$store.state.form.isLoading;
+  }
+
+  get questionTypeList(): QuestionTypeObject[] {
+    return this.$store.state.form.questionTypeList;
+  }
+
+  /* ------------------------------------
+  => Methods
+  ------------------------------------ */
+  close(): void {
+    const addQuestionForm = this.$refs.addQuestionForm as VForm;
+    this.closeDialog();
+    addQuestionForm.reset();
+  }
+  addQuestion(): void {
+    const addQuestionForm = this.$refs.addQuestionForm as VForm;
+    if (addQuestionForm.validate()) {
+      const data: Question = {
+        key: this.questionKey,
+        required: this.currentQuestion.required,
+        type: this.currentQuestion.type,
+        text: this.currentQuestion.text,
+        options: this.currentQuestion.options
+      };
+      this.add(data);
+      this.closeDialog();
+      addQuestionForm.reset();
+    }
+  }
+  editQuestion(): void {
+    const addQuestionForm = this.$refs.addQuestionForm as VForm;
+    if (addQuestionForm.validate()) {
+      const data: Question = {
+        key: this.currentQuestion.key,
+        required: this.currentQuestion.required,
+        type: this.currentQuestion.type,
+        text: this.currentQuestion.text,
+        options: this.currentQuestion.options
+      };
+      console.warn('data', data);
+      this.edit(data);
+      this.closeDialog();
+      addQuestionForm.reset();
+    }
+  }
+  notEmpty(identifier: string): any[] {
+    return notEmptyRules(identifier);
+  }
+  /* ------------------------------------
+  => Mounted (Lifecycle)
+  ------------------------------------ */
+  // async mounted(): Promise<void> {
+  //   await console.warn('questionTypeList', this.questionTypeList);
+  // }
+
+  /* ------------------------------------
+  => Watcher
+  ------------------------------------ */
+  @Watch('dialog')
+  async handleOnLoad(newValue: boolean): Promise<void> {
+    if (newValue) {
+      if (this.isEdit) {
+        console.warn('test #1', { ...this.selectedQuestion });
+        this.currentQuestion = { ...this.selectedQuestion };
+        console.warn('test #2', this.currentQuestion);
+      } else {
+        this.currentQuestion.required = false;
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.v-btn {
+  letter-spacing: normal;
+  text-transform: none;
+}
+// Put red colored asterisk on required class
+.required >>> label::after {
+  content: ' *';
+  color: red;
+}
+</style>
