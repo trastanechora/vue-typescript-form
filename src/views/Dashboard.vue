06fde420ab9b874e059521a5399f863297aefc80
trastanechora <trastanechora@gmail.com>
2021-03-13 05:21:04 +0700

    Update dashboard page

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,19 +1,40 @@
 <template>
-  <v-layout class="admin-container">
-    <v-layout wrap class="admin-view full-width">
-      <v-flex xs12 class="mb-4">
-        <v-btn rounded color="primary" outlined to="/dashboard/add"> <v-icon left>mdi-plus</v-icon>Tambah Form </v-btn>
-      </v-flex>
-      <v-flex xs12> </v-flex>
-    </v-layout>
+  <v-layout wrap class="admin-view full-width">
+    <v-flex xs12 class="mb-4">
+      <v-btn rounded color="primary" outlined to="/dashboard/add"> <v-icon left>mdi-plus</v-icon>Tambah Form </v-btn>
+    </v-flex>
+    <v-flex xs12>
+      <v-data-table :headers="formHeaders" :items="[]" :items-per-page="10" class="elevation-1 full-width">
+        <template v-slot:[`item.userAction`]="{ item }">
+          <v-tooltip right>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                icon
+                :loading="isLoading"
+                :disabled="isLoading"
+                v-bind="attrs"
+                v-on="on"
+                @click="showChartEditDialog(item)"
+              >
+                <v-icon>mdi-pencil</v-icon>
+              </v-btn>
+            </template>
+            <span>Edit</span>
+          </v-tooltip>
+        </template>
+      </v-data-table>
+    </v-flex>
   </v-layout>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { TableHeader, Form } from '@/@types';
+import AppBar from '@/components/AppBar.vue';
 
-@Component
+@Component({
+  components: { AppBar }
+})
 export default class DashboardPage extends Vue {
   /* ------------------------------------
   => Local State Declaration
@@ -39,7 +60,7 @@ export default class DashboardPage extends Vue {
   ** (Adopt store variables to local state)
   ------------------------------------ */
   get isLoading(): boolean {
-    return this.$store.state.user.isLoading;
+    return this.$store.state.form.isLoading;
   }
 
   get formList(): Form[] {
