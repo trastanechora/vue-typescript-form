10c6671395a6c12cedff0e1b39fcf0e663ed20e8
trastanechora <trastanechora@gmail.com>
2021-03-26 00:13:38 +0700

    Add status formatter function

--- src/@utils/formatter.ts
+++ src/@utils/formatter.ts
@@ -1,5 +1,38 @@
+import { FormStatus } from '@/@types';
+
 export const dateFormatter = (date: Date, monthFormat: string = 'long'): string => {
   const options: any = { year: 'numeric', month: monthFormat, day: 'numeric' };
   const newDate = new Date(date);
   return newDate.toLocaleDateString('id-ID', options);
+};
+
+export const statusFormatter = (status: FormStatus): any => {
+  switch (status) {
+    case FormStatus.OPEN:
+      return {
+        text: 'Terbuka',
+        icon: 'mdi-circle',
+        color: 'green'
+      };
+    case FormStatus.CLOSED:
+      return {
+        text: 'Tertutup',
+        icon: 'mdi-close-circle',
+        color: 'red'
+      };
+    case FormStatus.ENDED:
+      return {
+        text: 'Waktu Habis',
+        icon: 'mdi-circle-slice-3',
+        color: 'orange'
+      };
+    case FormStatus.DELETED:
+      return {
+        text: 'Dihapus',
+        icon: 'mdi-delete-circle',
+        color: 'grey'
+      };
+    default:
+      break;
+  }
 };
