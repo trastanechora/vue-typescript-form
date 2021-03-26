0e69a03bd8208d8293dccb84890d77130341dd8e
trastanechora <trastanechora@gmail.com>
2021-03-27 04:51:40 +0700

    Modify .d.ts file

--- src/@types/shims-vue.d.ts
+++ src/@types/shims-vue.d.ts
@@ -3,3 +3,13 @@ declare module '*.vue' {
   import Vue from 'vue';
   export default Vue;
 }
+
+declare module 'vuetify/lib/framework' {
+  import Vuetify from 'vuetify';
+  export default Vuetify;
+}
+
+declare module 'vue-json-to-csv' {
+  import VueJsonToCsv from 'vue-json-to-csv';
+  export default VueJsonToCsv;
+}
--- src/@types/shims-vuetify.d.ts
+++
@@ -1,5 +0,0 @@
-// This file exist only to make Vuetify works with Typescript
-declare module 'vuetify/lib/framework' {
-  import Vuetify from 'vuetify';
-  export default Vuetify;
-}
