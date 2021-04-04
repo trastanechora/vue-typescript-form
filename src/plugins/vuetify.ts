3582666437b39335bbb139c8cdaaf17afa497d24
trastanechora <trastanechora@gmail.com>
2021-04-04 08:05:18 +0700

    Add tone color for vuetify theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -18,7 +18,7 @@ export default new Vuetify({
         info: '#2196F3',
         success: '#4CAF50',
         warning: '#FFC107',
-        tone: '#f5f5f5;'
+        tone: '#f5f5f5'
       }
     }
   }
