633f8b45f8ae057e9d4332008c92435579351e0c
trastanechora <trastanechora@gmail.com>
2021-04-16 16:04:02 +0700

    Add pagination and section feature both

--- src/views/AddForm.vue
+++ src/views/AddForm.vue
@@ -163,128 +163,163 @@
               <hr />
             </v-flex>
             <!-- QUESTION LIST -->
-            <v-expansion-panels
-              class="full-width mb-4 section-title"
-              v-for="(questionSection, index) in formData.questions"
-              :key="index"
-            >
-              <h4 v-if="formData.questions.length > 1" class="primary--text mb-2">
-                {{ questionSection.title + (index + 1) }}
+            <div v-for="(questionPage, pageIndex) in formData.questions" :key="pageIndex" class="full-width">
+              <h3 v-if="formData.questions.length > 1" class="primary--text mb-2 text-center">
+                {{ questionPage.title + (pageIndex + 1) }}
                 <v-btn
-                  v-if="index > 0"
+                  v-if="pageIndex > 0"
                   color="primary"
                   class="ml-2"
                   outlined
                   rounded
-                  @click="removeSection(questionSection, index)"
+                  @click="removePage(questionPage, pageIndex)"
                   x-small
                 >
                   <v-icon left small>mdi-close-circle</v-icon> Hapus
                 </v-btn>
-              </h4>
-              <draggable
-                v-model="questionSection.questionList"
-                group="questions"
-                @start="dragState = true"
-                @end="dragState = false"
-                tag="div"
-                class="full-width"
+              </h3>
+              <v-expansion-panels
+                class="full-width mb-4 section-title"
+                v-for="(questionSection, sectionIndex) in questionPage.sectionList"
+                :key="sectionIndex"
               >
-                <v-expansion-panel v-for="item in questionSection.questionList" :key="item.key" class="full-width">
-                  <v-expansion-panel-header v-slot="{ open }">
-                    <v-row no-gutters>
-                      <v-col cols="4">
-                        {{ item.text }}
-                      </v-col>
-                      <v-col cols="8" class="text--secondary">
-                        <v-fade-transition leave-absolute>
-                          <v-row v-if="!open" no-gutters style="width: 100%">
-                            <v-col cols="6">{{ item.type.label }}</v-col>
-                            <v-col cols="6">{{ item.required ? 'Wajib Diisi' : '' }}</v-col>
+                <h4 v-if="questionPage.sectionList.length > 1" class="primary--text mb-2">
+                  {{ questionSection.title + (sectionIndex + 1) }}
+                  <v-btn
+                    v-if="sectionIndex > 0"
+                    color="primary"
+                    class="ml-2"
+                    outlined
+                    rounded
+                    @click="removeSection(questionSection, sectionIndex, pageIndex)"
+                    x-small
+                  >
+                    <v-icon left small>mdi-close-circle</v-icon> Hapus
+                  </v-btn>
+                </h4>
+                <draggable
+                  v-model="questionSection.questionList"
+                  group="questions"
+                  @start="dragState = true"
+                  @end="dragState = false"
+                  tag="div"
+                  class="full-width"
+                >
+                  <v-expansion-panel v-for="item in questionSection.questionList" :key="item.key" class="full-width">
+                    <v-expansion-panel-header v-slot="{ open }">
+                      <v-row no-gutters>
+                        <v-col cols="4">
+                          {{ item.text }}
+                        </v-col>
+                        <v-col cols="8" class="text--secondary">
+                          <v-fade-transition leave-absolute>
+                            <v-row v-if="!open" no-gutters style="width: 100%">
+                              <v-col cols="6">{{ item.type.label }}</v-col>
+                              <v-col cols="6">{{ item.required ? 'Wajib Diisi' : '' }}</v-col>
+                            </v-row>
+                          </v-fade-transition>
+                        </v-col>
+                      </v-row>
+                    </v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                      <v-row
+                        ><v-col cols="6" class="text--secondary px-6">
+                          <v-row v-if="item.image">
+                            <v-img :src="item.image" max-width="300" max-height="150"></v-img>
                           </v-row>
-                        </v-fade-transition>
-                      </v-col>
-                    </v-row>
-                  </v-expansion-panel-header>
-                  <v-expansion-panel-content>
-                    <v-row
-                      ><v-col cols="6" class="text--secondary px-6">
-                        <v-row v-if="item.image">
-                          <v-img :src="item.image" max-width="300" max-height="150"></v-img>
-                        </v-row>
-                        <v-row>{{ item.description ? item.description : '' }}</v-row>
-                        <v-row v-if="item.type.value === 'radio' || item.type.value === 'checkbox'"
-                          ><v-list dense>
-                            <v-subheader>Pilihan Jawaban:</v-subheader>
-                            <v-list-item-group color="primary">
-                              <v-list-item v-for="(option, optionIndex) in item.options" :key="optionIndex" disabled>
-                                <v-list-item-icon>
-                                  {{ optionIndex + 1 }}
-                                </v-list-item-icon>
-                                <v-list-item-content>
-                                  <v-list-item-title v-text="option.text"></v-list-item-title>
-                                </v-list-item-content>
-                              </v-list-item>
-                            </v-list-item-group> </v-list
-                        ></v-row>
-                      </v-col>
-                      <v-col cols="6" class="text-right">
-                        <v-chip class="ma-2"> {{ item.type.label }} </v-chip
-                        ><v-chip v-if="item.required" class="ma-2" color="error" outlined>
-                          Wajib Diisi
-                        </v-chip></v-col
-                      ></v-row
-                    >
-                    <v-row class="mx-2 mb-1">
-                      <v-spacer /><v-btn color="primary" text @click="openEditQuestionDialog(item)">
-                        <v-icon left>mdi-pencil</v-icon> Edit
-                      </v-btn>
-                      <v-btn color="primary" text @click="deleteQuestion(item)">
-                        <v-icon left>mdi-delete</v-icon> Hapus
-                      </v-btn></v-row
-                    >
-                  </v-expansion-panel-content>
-                </v-expansion-panel>
-              </draggable>
-            </v-expansion-panels>
+                          <v-row>{{ item.description ? item.description : '' }}</v-row>
+                          <v-row v-if="item.type.value === 'radio' || item.type.value === 'checkbox'"
+                            ><v-list dense>
+                              <v-subheader>Pilihan Jawaban:</v-subheader>
+                              <v-list-item-group color="primary">
+                                <v-list-item v-for="(option, optionIndex) in item.options" :key="optionIndex" disabled>
+                                  <v-list-item-icon>
+                                    {{ optionIndex + 1 }}
+                                  </v-list-item-icon>
+                                  <v-list-item-content>
+                                    <v-list-item-title v-text="option.text"></v-list-item-title>
+                                  </v-list-item-content>
+                                </v-list-item>
+                              </v-list-item-group> </v-list
+                          ></v-row>
+                        </v-col>
+                        <v-col cols="6" class="text-right">
+                          <v-chip class="ma-2"> {{ item.type.label }} </v-chip
+                          ><v-chip v-if="item.required" class="ma-2" color="error" outlined>
+                            Wajib Diisi
+                          </v-chip></v-col
+                        ></v-row
+                      >
+                      <v-row class="mx-2 mb-1">
+                        <v-spacer /><v-btn color="primary" text @click="openEditQuestionDialog(item)">
+                          <v-icon left>mdi-pencil</v-icon> Edit
+                        </v-btn>
+                        <v-btn color="primary" text @click="deleteQuestion(item)">
+                          <v-icon left>mdi-delete</v-icon> Hapus
+                        </v-btn></v-row
+                      >
+                    </v-expansion-panel-content>
+                  </v-expansion-panel>
+                </draggable>
+              </v-expansion-panels>
+            </div>
             <v-flex xs12 class="my-6 form-actions">
               <v-card flat>
                 <v-card-text>
                   <v-row justify="space-between" class="ma-0 pa-0">
-                    <v-btn
-                      rounded
-                      color="primary"
-                      outlined
-                      @click="openAddQuestionDialog"
-                      :disabled="isLoading"
-                      :loading="isLoading"
-                    >
-                      <v-icon left>mdi-plus</v-icon>Tambah Pertanyaan
-                    </v-btn>
-                    <v-btn
-                      color="primary"
-                      text
-                      @click="addSection"
-                      class="ml-3"
-                      :disabled="isLoading"
-                      :loading="isLoading"
-                    >
-                      <v-icon left>mdi-plus</v-icon>Tambah Bagian
-                    </v-btn>
+                    <v-layout wrap>
+                      <v-flex xs12>
+                        <v-btn
+                          color="primary"
+                          small
+                          text
+                          @click="addSection"
+                          :disabled="isLoading"
+                          :loading="isLoading"
+                        >
+                          <v-icon left small>mdi-plus</v-icon>Tambah Bagian
+                        </v-btn>
+                        <v-btn color="primary" small text @click="addPage" :disabled="isLoading" :loading="isLoading">
+                          <v-icon left small>mdi-plus</v-icon>Tambah Halaman
+                        </v-btn>
+                      </v-flex>
+                      <v-flex xs12>
+                        <v-btn
+                          rounded
+                          color="primary"
+                          outlined
+                          @click="openAddQuestionDialog"
+                          :disabled="isLoading"
+                          :loading="isLoading"
+                        >
+                          <v-icon left>mdi-plus</v-icon>Tambah Pertanyaan
+                        </v-btn>
+                      </v-flex>
+                    </v-layout>
                     <v-spacer />
-                    <v-switch v-if="isEdit" v-model="status" label="Status" class="mt-0 pt-1 pr-5" />
-                    <v-btn
-                      v-if="isEdit"
-                      rounded
-                      color="primary"
-                      @click="editForm"
-                      :disabled="isLoading"
-                      :loading="isLoading"
-                      >Perbarui Form</v-btn
-                    >
-                    <v-btn v-else rounded color="primary" @click="saveForm" :disabled="isLoading" :loading="isLoading"
-                      >Simpan Form</v-btn
-                    >
+                    <v-layout wrap>
+                      <v-flex xs12>
+                        <v-switch v-if="isEdit" v-model="status" label="Status" class="mt-0 pt-1 pr-5" />
+                        <v-btn
+                          v-if="isEdit"
+                          rounded
+                          color="primary"
+                          @click="editForm"
+                          :disabled="isLoading"
+                          :loading="isLoading"
+                          >Perbarui Form</v-btn
+                        >
+                        <v-btn
+                          v-else
+                          rounded
+                          color="primary"
+                          @click="saveForm"
+                          :disabled="isLoading"
+                          :loading="isLoading"
+                          >Simpan Form</v-btn
+                        >
+                      </v-flex>
+                    </v-layout>
                   </v-row>
                 </v-card-text>
               </v-card>
@@ -310,7 +345,7 @@ import { Component, Vue } from 'vue-property-decorator';
 import { uuid } from 'vue-uuid';
 import draggable from 'vuedraggable';
 
-import { Form, FormStatus, Question, QuestionSection, QuestionType } from '@/@types';
+import { Form, FormStatus, Question, QuestionSection, QuestionType, QuestionPage } from '@/@types';
 import { notEmptyRules } from '@/@utils';
 
 import AddQuestion from '@/components/AddQuestion.vue';
@@ -354,8 +389,13 @@ export default class AddFormPage extends Vue {
     link: '',
     questions: [
       {
-        title: 'Bagian ',
-        questionList: []
+        title: 'Halaman ',
+        sectionList: [
+          {
+            title: 'Bagian ',
+            questionList: []
+          }
+        ]
       }
     ],
     respondents: []
@@ -410,19 +450,38 @@ export default class AddFormPage extends Vue {
     this.dialog = true;
   }
   addQuestion(item: Question): void {
-    this.formData.questions[this.formData.questions.length - 1].questionList.push(item);
+    this.formData.questions[this.formData.questions.length - 1].sectionList[
+      this.formData.questions[this.formData.questions.length - 1].sectionList.length - 1
+    ].questionList.push(item);
+  }
+  addPage(): void {
+    this.formData.questions.push({
+      title: 'Halaman ',
+      sectionList: [
+        {
+          title: 'Bagian ',
+          questionList: []
+        }
+      ]
+    });
+  }
+  removePage(questionPage: QuestionPage, pageIndex: number): void {
+    questionPage.sectionList.forEach(section => {
+      this.formData.questions[pageIndex - 1].sectionList.push(section);
+    });
+    this.formData.questions.splice(pageIndex, 1);
   }
   addSection(): void {
-    this.formData.questions.push({
+    this.formData.questions[this.formData.questions.length - 1].sectionList.push({
       title: 'Bagian ',
       questionList: []
     });
   }
-  removeSection(section: QuestionSection, index: number): void {
+  removeSection(section: QuestionSection, sectionIndex: number, pageIndex: number): void {
     section.questionList.forEach(question => {
-      this.formData.questions[index - 1].questionList.push(question);
+      this.formData.questions[pageIndex].sectionList[sectionIndex - 1].questionList.push(question);
     });
-    this.formData.questions.splice(index, 1);
+    this.formData.questions[pageIndex].sectionList.splice(sectionIndex, 1);
   }
   saveForm(): void {
     const data = { ...this.formData };
@@ -505,30 +564,39 @@ export default class AddFormPage extends Vue {
     if (this.formData.imageBanner) {
       this.posterProvided = true;
     }
+    console.warn(this.formData);
   }
   getQuestionCount(): number {
     let newQuestionCount: number = 0;
-    this.formData.questions.forEach((quesitonSection: QuestionSection) => {
-      quesitonSection.questionList.forEach(() => {
-        newQuestionCount++;
+    this.formData.questions.forEach((questionPage: QuestionPage) => {
+      questionPage.sectionList.forEach((questionSection: QuestionSection) => {
+        questionSection.questionList.forEach(() => {
+          newQuestionCount++;
+        });
       });
     });
     return newQuestionCount;
   }
   deleteQuestion(selectedQuestion: Question): void {
-    const newQuestionSection = this.formData.questions.map((quesitonSection: QuestionSection) => {
-      const newQuestionList = quesitonSection.questionList.reduce(function(result: Question[], question: Question) {
-        if (question.key !== selectedQuestion.key) {
-          result.push(question);
-        }
-        return result;
-      }, []);
+    const newQuestionPage = this.formData.questions.map((questionPage: QuestionPage) => {
+      const newQuestionSection = questionPage.sectionList.map((quesitonSection: QuestionSection) => {
+        const newQuestionList = quesitonSection.questionList.reduce((result: Question[], question: Question) => {
+          if (question.key !== selectedQuestion.key) {
+            result.push(question);
+          }
+          return result;
+        }, []);
+        return {
+          title: quesitonSection.title,
+          questionList: newQuestionList
+        };
+      });
       return {
-        title: quesitonSection.title,
-        questionList: newQuestionList
+        title: questionPage.title,
+        sectionList: newQuestionSection
       };
     });
-    this.formData.questions = newQuestionSection;
+    this.formData.questions = newQuestionPage;
   }
   openEditQuestionDialog(selectedQuestion: Question): void {
     this.selectedQuestion = selectedQuestion;
@@ -536,21 +604,26 @@ export default class AddFormPage extends Vue {
     this.dialog = true;
   }
   editQuestion(editedQuestion: Question): void {
-    console.warn('editedQuestion', editedQuestion);
-    const newQuestionSection = this.formData.questions.map((quesitonSection: QuestionSection) => {
-      const newQuestionList = quesitonSection.questionList.map((question: Question) => {
-        if (question.key === editedQuestion.key) {
-          return editedQuestion;
-        } else {
-          return question;
-        }
+    const newQuestionPage = this.formData.questions.map((questionPage: QuestionPage) => {
+      const newQuestionSection = questionPage.sectionList.map((quesitonSection: QuestionSection) => {
+        const newQuestionList = quesitonSection.questionList.map((question: Question) => {
+          if (question.key === editedQuestion.key) {
+            return editedQuestion;
+          } else {
+            return question;
+          }
+        });
+        return {
+          title: quesitonSection.title,
+          questionList: newQuestionList
+        };
       });
       return {
-        title: quesitonSection.title,
-        questionList: newQuestionList
+        title: questionPage.title,
+        sectionList: newQuestionSection
       };
     });
-    this.formData.questions = newQuestionSection;
+    this.formData.questions = newQuestionPage;
   }
 
   async saveImageBanner(file: any): Promise<void> {
