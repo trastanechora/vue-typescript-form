725ffa4c7daae5a9e518c932005f1682b679eb7b
trastanechora <trastanechora@gmail.com>
2021-05-05 23:31:46 +0700

    Add image preview dialog component

---
+++ src/components/ImagePreview.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-dialog v-model="dialog" transition="dialog-bottom-transition" max-width="600">
+    <template>
+      <v-card>
+        <v-img :src="image"></v-img>
+        <v-card-actions class="justify-end">
+          <v-btn text @click="closeDialog">Tutup</v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class NameChangeDialog extends Vue {
+  /* ------------------------------------
+  => Props declaration
+  ------------------------------------ */
+  @Prop({ required: true, type: Boolean })
+  readonly dialog!: boolean;
+  @Prop({ required: true, type: String })
+  readonly image!: string;
+  @Prop({
+    required: false,
+    type: Function,
+    default() {
+      return true;
+    }
+  })
+  readonly closeDialog!: FunctionConstructor;
+
+  /* ------------------------------------
+  => Setter and Getter
+  ** (Adopt store variables to local state)
+  ------------------------------------ */
+  get isLoading(): boolean {
+    return this.$store.state.form.isLoading;
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.v-btn {
+  letter-spacing: normal;
+  text-transform: none;
+}
+</style>
