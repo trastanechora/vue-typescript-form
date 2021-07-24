343d6072836ae9c174618918597feaef4d9e53f6
Maestro Trastanechora <trastanechora@gmail.com>
2021-07-24 09:50:20 +0700

    Implement search on form page

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,144 +1,158 @@
 <template>
   <v-layout wrap class="pa-10">
-    <v-flex xs12 class="mb-4">
-      <v-btn rounded color="primary" outlined @click="addForm"> <v-icon left>mdi-plus</v-icon>Tambah Form </v-btn>
-      <v-btn rounded color="primary" outlined @click="importForm" class="ml-2">
-        <v-icon left small>mdi-upload</v-icon>Import</v-btn
-      >
-    </v-flex>
     <v-flex xs12>
-      <v-data-table
-        :headers="formHeaders"
-        :items="formList"
-        :items-per-page="10"
-        :loading="isLoading"
-        class="elevation-1"
-      >
-        <template v-slot:[`item.label`]="{ item }">
-          <v-tooltip right>
-            <template v-slot:activator="{ on }">
-              <v-btn text small color="primary" v-on="on" @click="toResponseList(item.uuid)">{{ item.label }}</v-btn>
-            </template>
-            <span>Lihat Responden</span>
-          </v-tooltip>
-        </template>
-        <template v-slot:[`item.status`]="{ item }">
-          <v-chip
-            v-if="checkStartDate(item.startDate)"
-            :color="statusColorFormatter('unstarted')"
-            class="ma-2"
-            outlined
-            small
+      <v-card>
+        <v-card-title>
+          <h3 class="primary--text">List Form</h3>
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="searchString"
+            append-icon="mdi-magnify"
+            label="Cari berdasarkan label"
+            clearable
+          ></v-text-field>
+          <v-btn small rounded color="primary" outlined @click="importForm" class="ml-2">
+            <v-icon left small>mdi-upload</v-icon>Import</v-btn
           >
-            <v-icon left x-small>
-              {{ statusIconFormatter('unstarted') }}
-            </v-icon>
-            {{ statusTextFormatter('unstarted') }}
-          </v-chip>
-          <v-chip
-            v-else-if="checkDueDate(item.dueDate)"
-            :color="statusColorFormatter('ended')"
-            class="ma-2"
-            outlined
-            small
-          >
-            <v-icon left x-small>
-              {{ statusIconFormatter('ended') }}
-            </v-icon>
-            {{ statusTextFormatter('ended') }}
-          </v-chip>
-          <v-chip v-else :color="statusColorFormatter(item.status)" class="ma-2" outlined small>
-            <v-icon left x-small>
-              {{ statusIconFormatter(item.status) }}
-            </v-icon>
-            {{ statusTextFormatter(item.status) }}
-          </v-chip>
-        </template>
-        <template v-slot:[`item.startDate`]="{ item }">
-          {{ item.startDate ? formatDate(item.startDate) : '-' }}
-        </template>
-        <template v-slot:[`item.dueDate`]="{ item }">
-          {{ item.dueDate ? formatDate(item.dueDate) : '-' }}
-        </template>
-        <template v-slot:[`item.createdAt`]="{ item }">
-          {{ formatDate(item.createdAt) }}
-        </template>
-        <template v-slot:[`item.updatedAt`]="{ item }">
-          {{ item.updatedAt ? formatDate(item.updatedAt) : '-' }}
-        </template>
-        <template v-slot:[`item.link`]="{ item }">
-          <v-tooltip right>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                icon
-                :loading="isLoading"
-                :disabled="isLoading"
-                v-bind="attrs"
-                v-on="on"
-                @click="copyToClipboard(item.link)"
-              >
-                <v-icon small>mdi-content-copy</v-icon>
-              </v-btn>
-            </template>
-            <span>Salin Tautan</span>
-          </v-tooltip>
-          <v-tooltip right>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                icon
-                :loading="isLoading"
-                :disabled="isLoading"
-                v-bind="attrs"
-                v-on="on"
-                @click="openNewTab(item.link)"
-              >
-                <v-icon>mdi-share</v-icon>
-              </v-btn>
-            </template>
-            <span>Buka</span>
-          </v-tooltip>
-        </template>
-        <template v-slot:[`item.userAction`]="{ item }">
-          <v-tooltip right>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn icon :loading="isLoading" :disabled="isLoading" v-bind="attrs" v-on="on" @click="editForm(item)">
-                <v-icon>mdi-pencil</v-icon>
-              </v-btn>
-            </template>
-            <span>Ubah</span>
-          </v-tooltip>
-          <v-tooltip right>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                icon
-                :loading="isLoading"
-                :disabled="isLoading"
-                v-bind="attrs"
-                v-on="on"
-                @click="duplicateForm(item)"
-              >
-                <v-icon small>mdi-content-duplicate</v-icon>
-              </v-btn>
-            </template>
-            <span>Gandakan Form</span>
-          </v-tooltip>
-          <v-tooltip right>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                icon
-                :loading="isLoading"
-                :disabled="isLoading"
-                v-bind="attrs"
-                v-on="on"
-                @click="showDeleteDialog(item)"
-              >
-                <v-icon>mdi-trash-can</v-icon>
-              </v-btn>
-            </template>
-            <span>Hapus</span>
-          </v-tooltip>
-        </template>
-      </v-data-table>
+          <v-btn small rounded color="primary" outlined @click="addForm" class="ml-2">
+            <v-icon left small>mdi-plus</v-icon>Tambah Form
+          </v-btn>
+        </v-card-title>
+        <hr color="gray" />
+        <v-data-table
+          :headers="formHeaders"
+          :items="formList"
+          :items-per-page="10"
+          :search="searchString"
+          :loading="isLoading"
+          class="mt-4"
+        >
+          <template v-slot:[`item.label`]="{ item }">
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-btn text small color="primary" v-on="on" @click="toResponseList(item.uuid)">{{ item.label }}</v-btn>
+              </template>
+              <span>Lihat Responden</span>
+            </v-tooltip>
+          </template>
+          <template v-slot:[`item.status`]="{ item }">
+            <v-chip
+              v-if="checkStartDate(item.startDate)"
+              :color="statusColorFormatter('unstarted')"
+              class="ma-2"
+              outlined
+              small
+            >
+              <v-icon left x-small>
+                {{ statusIconFormatter('unstarted') }}
+              </v-icon>
+              {{ statusTextFormatter('unstarted') }}
+            </v-chip>
+            <v-chip
+              v-else-if="checkDueDate(item.dueDate)"
+              :color="statusColorFormatter('ended')"
+              class="ma-2"
+              outlined
+              small
+            >
+              <v-icon left x-small>
+                {{ statusIconFormatter('ended') }}
+              </v-icon>
+              {{ statusTextFormatter('ended') }}
+            </v-chip>
+            <v-chip v-else :color="statusColorFormatter(item.status)" class="ma-2" outlined small>
+              <v-icon left x-small>
+                {{ statusIconFormatter(item.status) }}
+              </v-icon>
+              {{ statusTextFormatter(item.status) }}
+            </v-chip>
+          </template>
+          <template v-slot:[`item.startDate`]="{ item }">
+            {{ item.startDate ? formatDate(item.startDate) : '-' }}
+          </template>
+          <template v-slot:[`item.dueDate`]="{ item }">
+            {{ item.dueDate ? formatDate(item.dueDate) : '-' }}
+          </template>
+          <template v-slot:[`item.createdAt`]="{ item }">
+            {{ formatDate(item.createdAt) }}
+          </template>
+          <template v-slot:[`item.updatedAt`]="{ item }">
+            {{ item.updatedAt ? formatDate(item.updatedAt) : '-' }}
+          </template>
+          <template v-slot:[`item.link`]="{ item }">
+            <v-tooltip right>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  icon
+                  :loading="isLoading"
+                  :disabled="isLoading"
+                  v-bind="attrs"
+                  v-on="on"
+                  @click="copyToClipboard(item.link)"
+                >
+                  <v-icon small>mdi-content-copy</v-icon>
+                </v-btn>
+              </template>
+              <span>Salin Tautan</span>
+            </v-tooltip>
+            <v-tooltip right>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  icon
+                  :loading="isLoading"
+                  :disabled="isLoading"
+                  v-bind="attrs"
+                  v-on="on"
+                  @click="openNewTab(item.link)"
+                >
+                  <v-icon>mdi-share</v-icon>
+                </v-btn>
+              </template>
+              <span>Buka</span>
+            </v-tooltip>
+          </template>
+          <template v-slot:[`item.userAction`]="{ item }">
+            <v-tooltip right>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn icon :loading="isLoading" :disabled="isLoading" v-bind="attrs" v-on="on" @click="editForm(item)">
+                  <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+              </template>
+              <span>Ubah</span>
+            </v-tooltip>
+            <v-tooltip right>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  icon
+                  :loading="isLoading"
+                  :disabled="isLoading"
+                  v-bind="attrs"
+                  v-on="on"
+                  @click="duplicateForm(item)"
+                >
+                  <v-icon small>mdi-content-duplicate</v-icon>
+                </v-btn>
+              </template>
+              <span>Gandakan Form</span>
+            </v-tooltip>
+            <v-tooltip right>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  icon
+                  :loading="isLoading"
+                  :disabled="isLoading"
+                  v-bind="attrs"
+                  v-on="on"
+                  @click="showDeleteDialog(item)"
+                >
+                  <v-icon>mdi-trash-can</v-icon>
+                </v-btn>
+              </template>
+              <span>Hapus</span>
+            </v-tooltip>
+          </template>
+        </v-data-table>
+      </v-card>
     </v-flex>
     <DialogConfirmation :dialog="dialog" :close-dialog="closeDeleteDialog" />
   </v-layout>
@@ -158,6 +172,7 @@ export default class FormPage extends Vue {
   => Local State Declaration
   ------------------------------------ */
   dialog: boolean = false;
+  searchString: string = '';
   formHeaders: TableHeader[] = [
     { text: 'Label', value: 'label', width: 300 },
     { text: 'Jumlah Pertanyaan', value: 'questionCount', align: 'center' },
