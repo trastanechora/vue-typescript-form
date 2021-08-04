d4fbe1a71a9b3394bf9f50b792be1cc45e6497e5
trastanechora <trastanechora@gmail.com>
2021-08-04 18:23:42 +0700

    Modify forbidden page

--- src/views/Forbidden.vue
+++ src/views/Forbidden.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="my-12">
-    <h1 class="text-center">This page is limited access.</h1>
-  </div>
+  <v-layout wrap class="my-6 full-width">
+    <v-flex xs10 class="mt-10 mx-auto">
+      <v-card class="mx-auto py-5" width="100%">
+        <v-card-text>
+          <h1 class="text-center error--text">Halaman ini terlarang</h1>
+          <h4 class="text-center">Mohon login sebagai admin untuk dapat mengakses halaman ini</h4>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex xs10 class="my-2 mx-auto text-end">
+      <v-btn text small color="secondary" class="ml-2" to="/"
+        ><v-icon small>mdi-chevron-left</v-icon>Kembali ke Halaman Utama</v-btn
+      >
+    </v-flex>
+  </v-layout>
 </template>
 
 <script lang="ts">
@@ -10,3 +22,10 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component
 export default class ForbiddenPage extends Vue {}
 </script>
+
+<style scoped>
+.v-btn {
+  letter-spacing: normal;
+  text-transform: none;
+}
+</style>
